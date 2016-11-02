48f8bce7118c965b350a1b8ec46442dbb7bdbd4a
Ken Duigraha Putra <email": null>
2016-11-02 13:47:39 +0700

    refactor: change to multer library

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const path = require('path')
-var fileUpload = require('express-fileupload');
+// var fileUpload = require('express-fileupload');
 
 /* tambahan */
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     console.log(`test`);
-    cb(null, 'public/uploads')
+    cb(null, __dirname + '/public/uploads')
   },
   filename: function (req, file, cb) {
     cb(null, Date.now()) + '-' + file.originalname
@@ -20,7 +20,7 @@ var storage = multer.diskStorage({
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(fileUpload())
+// app.use(fileUpload())
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -28,35 +28,35 @@ app.use(bodyParser.json())
 app.use(cors())
 
 /* upload */
-app.post('/upload', function(req, res) {
-    var sampleFile;
+// app.post('/upload', function(req, res) {
+//     var sampleFile;
+//
+//     if (!req.files) {
+//         res.send('No files were uploaded.');
+//         return;
+//     }
+//
+//     sampleFile = req.files.sampleFile;
+//     console.log(sampleFile);
+//     sampleFile.mv(path.join(__dirname, './public/' + sampleFile.name), function(err) {
+//       console.log("masuk pindahan");
+//         if (err) {
+//             res.status(500).send(err);
+//         }
+//         else {
+//             res.send('File uploaded!');
+//         }
+//     });
+// });
 
-    if (!req.files) {
-        res.send('No files were uploaded.');
-        return;
-    }
-
-    sampleFile = req.files.sampleFile;
-    console.log(sampleFile);
-    sampleFile.mv(path.join(__dirname, './public/' + sampleFile.name), function(err) {
-      console.log("masuk pindahan");
-        if (err) {
-            res.status(500).send(err);
-        }
-        else {
-            res.send('File uploaded!');
-        }
-    });
-});
-
-// var upload = multer({ storage: storage }).single('photo')
-var upload = multer({dest: 'public/uploads/'})
+var upload = multer({ storage: storage }).single('photo')
+// var upload = multer({dest: 'public/uploads/'})
 
 /* test upload */
-app.post('/', upload.any() ,(req, res) => {
-  console.log(req.files);
-  res.send(req.files)
-})
+// app.post('/', upload.any() ,(req, res) => {
+//   console.log(req.files);
+//   res.send(req.files)
+// })
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/index.html`)
@@ -73,8 +73,6 @@ app.post('/api/upload', (req, res) => {
     console.log('Photo Uploaded');
   })
 });
-
-
 
 const hostname = process.env.HOST || "localhost"
 const port = process.env.PORT || "3000"
