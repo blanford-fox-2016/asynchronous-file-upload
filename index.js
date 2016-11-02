bfb5a7b98977f0b888bba02c0d09f0d481ad4090
Ken Duigraha Putra <email": null>
2016-11-02 09:35:10 +0700

    checkout

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const path = require('path')
+var fileUpload = require('express-fileupload');
+
+/* tambahan */
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    console.log(`test`);
     cb(null, 'public/uploads')
   },
   filename: function (req, file, cb) {
@@ -14,13 +19,44 @@ var storage = multer.diskStorage({
   }
 })
 
-var upload = multer({ storage: storage }).single('photo')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(fileUpload())
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors())
 
+/* upload */
+app.post('/upload', function(req, res) {
+    var sampleFile;
+
+    if (!req.files) {
+        res.send('No files were uploaded.');
+        return;
+    }
+
+    sampleFile = req.files.sampleFile;
+    console.log(sampleFile);
+    sampleFile.mv(path.join(__dirname, './public/' + sampleFile.name), function(err) {
+      console.log("masuk pindahan");
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.send('File uploaded!');
+        }
+    });
+});
+
+// var upload = multer({ storage: storage }).single('photo')
+var upload = multer({dest: 'public/uploads/'})
+
+/* test upload */
+app.post('/', upload.any() ,(req, res) => {
+  console.log(req.files);
+  res.send(req.files)
+})
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/index.html`)
@@ -36,7 +72,6 @@ app.post('/api/upload', (req, res) => {
     res.end('Your File Uploaded');
     console.log('Photo Uploaded');
   })
-  console.log(`test`);
 });
 
 
